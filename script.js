2c46ae53c53978554672eb8b9bffa321d5ee1c9c
MichalLekacz <87401492+MichalLekacz@users.noreply.github.com>
2023-11-06 23:36:32 +0000

    Update script.js
    testing for Safari

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
-function removeTransition(e) {
-  if (e.propertyName !== 'transform') return;
-  e.target.classList.remove('scale-110' , 'border-blue-500');
-}
+// Inicjalizacja AudioContext
+const audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
+// Funkcja do odtwarzania dźwięku
 function playSound(key) {
   const audio = document.querySelector(`audio[data-key="${key.dataset.key}"]`);
   if (!audio) return;
 
+  const source = audioContext.createBufferSource();
+
+  // Załaduj plik dźwiękowy
+  fetch(audio.getAttribute('src'))
+    .then(response => response.arrayBuffer())
+    .then(data => audioContext.decodeAudioData(data))
+    .then(audioBuffer => {
+      source.buffer = audioBuffer;
+      source.connect(audioContext.destination);
+      source.start(0);
+    });
+
   key.classList.add('scale-110', 'border-blue-500');
-  audio.currentTime = 0;
-  audio.play();
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  e.target.classList.remove('scale-110', 'border-blue-500');
 }
 
 function handleKeydown(e) {
   const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
   if (key) {
-      playSound(key);
+    playSound(key);
   }
 }
 
 function handleClick(e) {
   if (e.target.classList.contains('key')) {
-      playSound(e.target);
+    playSound(e.target);
   }
 }
 
