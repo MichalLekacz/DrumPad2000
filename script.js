954b49ce96a9dcb4e9db722ac0adde7fa477b441
MichalLekacz <m.lekacz@op.pl>
2023-11-07 11:01:57 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-let audioContext; // Inicjalizacja AudioContext
+let audioContext; // Initialize the AudioContext
+let audioBuffer = null; // Store the audio buffer for quick playback
 
 function initAudioContext() {
-  audioContext = new (window.AudioContext || window.webkitAudioContext)({
-    sampleRate: 44100,
-  });
+  audioContext = new (window.AudioContext || window.webkitAudioContext)();
+}
+
+function loadAudioBuffer(audioElement) {
+  return fetch(audioElement.getAttribute("src"))
+    .then((response) => response.arrayBuffer())
+    .then((data) => audioContext.decodeAudioData(data));
 }
 
 function playSound(key) {
   if (!audioContext) {
-    // Inicjalizuj AudioContext, jeśli jeszcze nie jest zainicjowany
     initAudioContext();
   }
 
-  const audio = document.querySelector(`audio[data-key="${key.dataset.key}"]`);
-  if (!audio) return;
+  const audioElement = document.querySelector(
+    `audio[data-key="${key.dataset.key}"]`
+  );
+  if (!audioElement) return;
 
-  // Załaduj plik dźwiękowy
-  fetch(audio.getAttribute("src"))
-    .then((response) => response.arrayBuffer())
-    .then((data) => audioContext.decodeAudioData(data))
+  loadAudioBuffer(audioElement)
     .then((audioBuffer) => {
       const source = audioContext.createBufferSource();
       source.buffer = audioBuffer;
@@ -26,77 +29,30 @@ function playSound(key) {
       source.start(0);
     })
     .catch((error) => {
-      console.error("Błąd podczas ładowania i odtwarzania dźwięku:", error);
+      console.error("Error loading and playing audio:", error);
     });
 
   key.classList.add("scale-110", "border-blue-500");
-}
-
-function removeTransition(e) {
-  if (e.propertyName !== "transform") return;
-  const key = e.target;
-  key.classList.remove("scale-110", "border-blue-500");
   setTimeout(() => {
     key.classList.remove("scale-110", "border-blue-500");
   }, 70); // Adjust the delay as needed
 }
 
-function handleKeydown(e) {
+// Event listeners
+const keys = Array.from(document.querySelectorAll(".key"));
+keys.forEach((key) => {
+  key.addEventListener("transitionend", removeTransition);
+  key.addEventListener("click", () => playSound(key));
+});
+
+window.addEventListener("keydown", (e) => {
   const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
   if (key) {
     playSound(key);
   }
-}
-
-function handleClick(e) {
-  if (e.target.classList.contains("key")) {
-    playSound(e.target);
-  }
-}
-
-const keys = Array.from(document.querySelectorAll(".key"));
-keys.forEach((key) => {
-  key.addEventListener("transitionend", removeTransition);
-  key.addEventListener("click", handleClick);
 });
 
-const activeTouches = new Set(); // To keep track of active touches
-
-function handleTouch(e) {
-  if (e.target.classList.contains("key")) {
-    const key = e.target;
-    if (!activeTouches.has(key)) {
-      activeTouches.add(key);
-      playSound(key);
-    }
-  }
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  e.target.classList.remove("scale-110", "border-blue-500");
 }
-
-function handleTouchEnd(e) {
-  if (e.target.classList.contains("key")) {
-    const key = e.target;
-    activeTouches.delete(key);
-  }
-}
-
-keys.forEach((key) => {
-  key.addEventListener("transitionend", removeTransition);
-  key.addEventListener("click", handleClick);
-  key.addEventListener("touchstart", handleTouch);
-  key.addEventListener("touchend", handleTouchEnd);
-});
-
-window.addEventListener("touchstart", () => {
-  if (!audioContext) {
-    initAudioContext();
-  }
-});
-
-// Inicjalizacja AudioContext po kliknięciu, jeśli jeszcze nie jest zainicjowany
-window.addEventListener("click", () => {
-  if (!audioContext) {
-    initAudioContext();
-  }
-});
-
-window.addEventListener("keydown", handleKeydown);
